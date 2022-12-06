16a5d71af7308c6abdc26e47882ec5a0109d64c0
Vladyslav Komar <valdek125@gmail.com>
2022-12-07 00:31:53 +0200

    Update creareArrOfAverage

--- src/utils/creareArrOfAverage.ts
+++ src/utils/creareArrOfAverage.ts
@@ -1,12 +1,13 @@
+import { IMatrix } from "../redux/matrix/types"
 
-export const creareArrOfAverage = (heigth:number, width:number, matrix:any ):number[] => {
+export const creareArrOfAverage = (heigth:number, width:number, matrix:IMatrix ):number[] => {
     const arrOfAverage:number[] = []
 
     for (let w = 0; w < width; w += 1) {
         let total: number = 0
 
         for (let h = 0; h < heigth; h += 1) {
-            total += matrix[h][w]
+            total += matrix[h].line[w].number
         }
 
         const average = +(total / heigth).toFixed(0)
